abd8604ecc3106c1f3001450e78519e44fe18a1d
seok-hyung <korea970223@naver.com>
2024-03-18 17:54:13 +0900

    :sparkles:Feat : 수량 조절 버튼 추가

--- app/list/page.js
+++ app/list/page.js
@@ -1,14 +1,30 @@
-import Image from 'next/image'
-import product0 from '/public/food0.png'
-import product1 from '/public/food1.png'
-import product2 from '/public/food2.png'
+'use client';
+import Image from 'next/image';
+import product0 from '/public/food0.png';
+import product1 from '/public/food1.png';
+import product2 from '/public/food2.png';
+import { useState } from 'react';
 
 export default function List() {
-  const products = [
-    { name: 'Tomatoes', price: 10000, image: product0 },
-    { name: 'Pasta', price: 20000, image: product1 },
-    { name: 'Coconut', price: 5000, image: product2 },
-  ]
+  const [products, setProducts] = useState([
+    { name: 'Tomatoes', price: 10000, image: product0, count: 1, stock: 10 },
+    { name: 'Pasta', price: 20000, image: product1, count: 1, stock: 8 },
+    { name: 'Coconut', price: 5000, image: product2, count: 1, stock: 5 },
+  ]);
+  const increaseCount = (index) => {
+    const newProducts = [...products];
+    if (newProducts[index].count < newProducts[index].stock) {
+      newProducts[index].count += 1;
+      setProducts(newProducts);
+    }
+  };
+  const decreaseCount = (index) => {
+    const newProducts = [...porudcts];
+    if (newProducts[index].count > 0) {
+      newProducts[index].count -= 1;
+      setProducts(newProducts);
+    }
+  };
   return (
     <>
       <div class="title">상품목록</div>
@@ -18,9 +34,24 @@ export default function List() {
             <Image src={product.image} className="food-img" alt="food-img" />
             <h4>{product.name}</h4>
             <p>{product.price}원</p>
+            <button
+              onClick={() => {
+                decreaseCount(index);
+              }}
+            >
+              -
+            </button>
+            <span>{product.count}</span>
+            <button
+              onClick={() => {
+                increaseCount(index);
+              }}
+            >
+              +
+            </button>
           </div>
-        )
+        );
       })}
     </>
-  )
+  );
 }
